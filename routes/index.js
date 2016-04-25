89af9d96b8f46eadce3670d69e82bf188cfb94a2
Joe Enos <joe@jtenos.com>
2016-04-25 20:14:03 +0000

    Starting the post to update the image

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var fs = require("fs-extra");
 
 var router = express.Router();
 
+var imagetext = require("../imagetext");
+
 var inputDir = path.join(
     path.join(
         path.dirname(require.main.filename), 
@@ -12,13 +14,61 @@ var inputDir = path.join(
     "inputimages"
 );
 
+var outputDir = path.join(
+    path.join(
+        path.dirname(require.main.filename),
+        "public"
+    ),
+    "outputimages"
+);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    fs.readdir(inputDir, function(err, files) {
       res.render('index', { 
-          title: 'Image Text Maker', 
-          query: req.query,
-          files: files
+          title: 'Image Text Maker'
+      });
+});
+
+
+router.post("/index/post-file", function(req, res, next) {
+    var fileName = path.join(inputDir, req.body.fileName);
+    var resultingFileName = path.join(outputDir, req.body.fileName);
+    var textItems = {
+        northwest: req.body.northwest,
+        northeast: req.body.northeast,
+        southwest: req.body.southwest,
+        southeast: req.body.southeast,
+        center: req.body.center
+    };
+    
+    imagetext.addAllText(fileName, resultingFileName, textItems, "#f60", function() {
+        res.json({success:true});
+    });
+});
+
+
+router.get("/index/get-files", function(req, res, next) {
+    fs.readdir(inputDir, function(err, files) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        
+      var fileInfos = [];
+      var stats;
+      files.forEach(function(file) {
+          var isDone;
+          try {
+              stats = fs.lstatSync(path.join(outputDir, file));
+                isDone = stats.isFile();
+          } catch (ex) {
+            isDone = false;
+          }
+          fileInfos.push({isDone: isDone, fileName: file});
+      });
+
+      res.render('filelist', { 
+          files: fileInfos
       });
     });
 });
